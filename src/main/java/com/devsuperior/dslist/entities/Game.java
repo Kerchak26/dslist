7456e5b34e6c838b21f101d278991b8c7ab474ef
Kauã Queiroga <queirogakaua@gmail.com>
2025-08-23 17:17:30 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -19,7 +19,11 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
-    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;    
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
